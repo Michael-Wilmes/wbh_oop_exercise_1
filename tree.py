ea553dd33889ae311767d19280df98cedf3ea170
Michael-Wilmes <micha.wilmes@outlook.com>
2024-11-18 17:57:17 +0100

    version without window

--- tree.py
+++ tree.py
@@ -1,10 +1,7 @@
 import sys
 import os
-import tkinter as tk
-import threading
-from tkinter import scrolledtext
 
-NEW_LINE = '\n'
+
 SPACE = ' '
 LAST_ENTRY = chr(9492)
 LINE = chr(9472)
@@ -13,22 +10,7 @@ CROSS = chr(9500)
 
 class DirectoryTree:
     
-    def __init__(self):
-
-        self.root = tk.Tk("Directory Tree")
-        self.root.title("Directory Tree Viewer")
-        self.root.geometry('800x800')
-        self.root.resizable(True, True)
-        self._text_widget = scrolledtext.ScrolledText(self.root, wrap = tk.WORD)
-        self._text_widget.tag_config("no_permission", background="white", foreground="red")
-
-    def show_window(self):
-        
-        self._text_widget.pack(expand = 1, fill = 'both')
-        self.root.mainloop()
-
-    def start(self, path=None):
-
+    def __init__(self, path=None ):
         #check source of path variable
         if ( len(sys.argv) < 2 and path == None):
             print("Usage: tree.py <path>")
@@ -39,45 +21,42 @@ class DirectoryTree:
         else:
             path = path 
 
-        #start in a thread, to avoid blocking the UI
-        threading.Thread(target=self.__scan_and_display, args=(path,)).start()
-        self.show_window()
+        self.__scan_and_display(path)
 
 
     def __scan_and_display(self, path):
         nfiles, ndirectories = self.__print_directory(path)
-        if nfiles == 0 and ndirectories == 0:
-            self._text_widget.insert(tk.END, "No files or directories found in " + os.path.abspath(path) + NEW_LINE)
+        if (nfiles == 0 and ndirectories == 0):
+            print(f"No files or directories found in {os.path.abspath(path)}")
             return
-
-        mes = NEW_LINE * 3 + str(nfiles) + " file(s), in " + str(ndirectories) + " directories  " + NEW_LINE
-        self._text_widget.insert(tk.END, mes)
-        self._text_widget.insert(tk.END, 'Starting directory: ' + os.path.abspath(path) + NEW_LINE)
-
+        
+        print("\n")
+        print(f"{str(nfiles)} file(s), in {str(ndirectories)} directories")
+        print(f"Starting directory: {os.path.abspath(path)}")
 
     def __print_directory(self, path, prefix_pattern = "", intendation_level = 0, is_last_directory = False):
 
         if (os.path.exists(path) == False):
-            self._text_widget.insert(tk.END, "Error: Path '" + os.path.abspath(path) +  "' can not be found! " + NEW_LINE)
+            print(f"Error: Path {os.path.abspath(path)} can not be found!")
             return (0,0)
 
-        if intendation_level == 0:
+        if (intendation_level == 0):
             os.chdir(path)
-            self._text_widget.insert(tk.END, os.path.abspath(path) + NEW_LINE)
+            print(f"{os.path.abspath(path)}")
         else:
-            if is_last_directory == True:
+            if (is_last_directory == True):
                 prefix_pattern += 3*SPACE
             else:
                 prefix_pattern += VERTICAL_LINE+ 2*SPACE
 
-        if os.access(path, os.F_OK) == False:
-            self._text_widget.insert(tk.END, prefix_pattern + LAST_ENTRY + " NO PERMISSION " + path.name + NEW_LINE, "no_permission")
+        if (os.access(path, os.F_OK) == False):
+            print(f"{prefix_pattern}{LAST_ENTRY} NO PERMISSION {path.name}")
             return (0,0)
 
         #get list of files and directories
         dir_list = self.__try_scan_dir(path, prefix_pattern)
 
-        if dir_list is None:
+        if (dir_list is None):
             return (0,0)
 
         #sort the list by name   
@@ -89,22 +68,21 @@ class DirectoryTree:
 
         for entry in sorted_list :
 
-            if entry.is_dir() or entry.is_file():             
+            if (entry.is_dir() or entry.is_file()):             
                 
                 #create line pattern for optical representation
                 line_pattern = ''
 
                 #special case for last file or directory
-                if entry == sorted_list[-1]:
+                if (entry == sorted_list[-1]):
                     is_last_directory = True
                     line_pattern =  LAST_ENTRY + LINE
                 else:
                     line_pattern = CROSS + LINE
 
-                self._text_widget.insert(tk.END, prefix_pattern +  line_pattern + SPACE +  entry.name + NEW_LINE)
-                #self.root.update_idletasks()  # Update the GUI
+                print(f"{prefix_pattern}{line_pattern}{SPACE}{entry.name}")
                 
-                if entry.is_dir():
+                if (entry.is_dir()):
                     ndirectories += 1
                     (sub_nfiles, sub_ndirectories) = self.__print_directory(entry, prefix_pattern, intendation_level + 1, is_last_directory)      
                     nfiles += sub_nfiles
@@ -120,42 +98,20 @@ class DirectoryTree:
         dir_list = None
         try:
             dir_list = os.scandir(path) 
-            if dir_list is None:
-                self._text_widget.insert(tk.END, prefix_pattern + LAST_ENTRY + " NO PERMISSION " + path.name + NEW_LINE, "no_permission")
+            if (dir_list is None):
+                print(f"{prefix_pattern}{LAST_ENTRY} NO PERMISSION { path.name}")
                 return None
         except PermissionError:
-            self._text_widget.insert(tk.END, prefix_pattern + LAST_ENTRY + " NO PERMISSION " + path.name + NEW_LINE, "no_permission")
+            print(f"{prefix_pattern}{LAST_ENTRY} NO PERMISSION { path.name}")
             return None
         except FileNotFoundError:
-            self._text_widget.insert(tk.END, prefix_pattern + LAST_ENTRY + " FILE NOT FOUND " + path.name + NEW_LINE, "no_permission")
+            print(f"{prefix_pattern}{LAST_ENTRY} FILE NOT FOUND { path.name}")
         except OSError:
-            self._text_widget.insert(tk.END, prefix_pattern + LAST_ENTRY + " OS ERROR " + path.name + NEW_LINE, "no_permission")
+            print(f"{prefix_pattern}{LAST_ENTRY} OS ERROR { path.name}")
             return None
         
         return dir_list
 
 
 if __name__ == "__main__":
-    tree = DirectoryTree()
-    tree.start("D:")
-    tree.show_window()
-
-
-#quellen: 
-#https://www.pythontutorial.net/
-#https://www.python.org/doc/
-#https://www.youtube.com/@patloeber
-#Python3 , das Umfassende Handbuch, Galileo Computing
-
-
- #todo: (gilt für alle Aufgaben)
- # Quellenangabe
- # Java ist auch eine Insel, Galileo Computing
- # https://www.youtube.com/@willtollefson
- # https://docs.oracle.com/en/java/
- #
- # auf probleme eingehen
- #  - Java Klammer Aufgabe ("zählt nur Klammer Paare") 
- #  - Java Koch Kurve: Darstellung bei Level > 5, Berechnung der Linien  bei Änderung der Punkte-Reihenfolge
-
-
+    DirectoryTree("D:")
